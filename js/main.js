c24eb22fec581f0bac6aed5a1fc0a456424d7419
lupesms97 <lupesms97@gmail.com>
2023-05-29 16:01:38 -0300

    feat: testimonials

--- js/main.js
+++ js/main.js
@@ -299,7 +299,30 @@ $(document).ready(function() {
       ).then((html) => {
         // you can put any JS code here
         alert('Seu email foi enviado com sucesso!')
-        window.location.href = '/education-master/index.html';
+        window.location.href = '../index.html';
+      });
+    });
+
+      //------- tESTIMONIALS --------//  
+
+    $(document).ready(function () {
+      $(".testimonial .indicators li").click(function () {
+        var i = $(this).index();
+        var targetElement = $(".testimonial .tabs li");
+        targetElement.eq(i).addClass("active");
+        targetElement.not(targetElement[i]).removeClass("active");
+      });
+      $(".testimonial .tabs li").click(function () {
+        var targetElement = $(".testimonial .tabs li");
+        targetElement.addClass("active");
+        targetElement.not($(this)).removeClass("active");
+      });
+    });
+    $(document).ready(function () {
+      $(".slider .swiper-pagination span").each(function (i) {
+        $(this)
+          .text(i + 1)
+          .prepend("0");
       });
     });
 
